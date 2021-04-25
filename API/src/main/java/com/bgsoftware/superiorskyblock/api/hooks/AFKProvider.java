9a8dcf75fa222b5f0709e4f5350ece297288837e
xSavior_of_God <66216419+xSavior-of-God@users.noreply.github.com>
2021-04-25 23:17:13 +0200

    Delete API directory

--- API/src/main/java/com/bgsoftware/superiorskyblock/api/hooks/AFKProvider.java
+++
@@ -1,9 +0,0 @@
-package com.bgsoftware.superiorskyblock.api.hooks;
-
-import org.bukkit.entity.Player;
-
-public interface AFKProvider {
-
-    boolean isAFK(Player player);
-
-}
--- API/src/main/java/com/bgsoftware/superiorskyblock/api/upgrades/cost/UpgradeCostLoadException.java
+++
@@ -1,9 +0,0 @@
-package com.bgsoftware.superiorskyblock.api.upgrades.cost;
-
-public final class UpgradeCostLoadException extends Exception {
-
-    public UpgradeCostLoadException(String message){
-        super(message);
-    }
-
-}
